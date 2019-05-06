9fd2aefc739fd1f1dfb28e8e6434dfc8c2cf7e8a
Veronica Mc Gray <veronicamcgray@veronicas-mbp.wifi.smc.edu>
2019-05-06 15:54:50 -0700

    1-0

---
+++ Assignment 10/canvas.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var c = canvas.getContext('2d');
+c.fillRect(100, 100, 100, 100); 
+c.fillRect(400, 100, 100, 100); 
+c.fillRect(300, 300, 100, 100); 
+console.log(canvas);
+//line
+c.beginPath();
+c.moveTo(50,300);
+c.lineTo(300,100);
+c.lineTo(400,300);
+c.stroke();
